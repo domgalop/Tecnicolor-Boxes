b1d7d872764ec35e674d8054d1935d86808fc8ca
Dom <25825490+domgalop@users.noreply.github.com>
2018-06-13 14:25:36 -0500

    organize code using eslint and airbnb ruleset

--- js/main.js
+++ js/main.js
@@ -1,47 +1,29 @@
-const div = document.getElementById('container');
-let bgChange = function() {
-  let fragment = document.createDocumentFragment(),
-  element = undefined,
-  divs = 100;
-
-  let createDivs = (param) => {
-    for (let i = 0; i < param; i++ ){
-      element = document.createElement('div');
+/** ------------ Version 2 ------------ */
+const div = document.getElementById("container");
+const tecniColor = (function tecniColor() {
+  const fragment = document.createDocumentFragment();
+  let element;
+  const divs = 100;
+  const createDivs = param => {
+    for (let i = 0; i < param; i += 1) {
+      element = document.createElement("div");
       element.classList.add("cube");
-      fragment.appendChild(element); 
+      fragment.appendChild(element);
     }
     div.appendChild(fragment);
   };
 
-  let bgChange = (e) => {
-    e.target.style.backgroundColor = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)}`;
+  const bgChange = e => {
+    e.target.style.backgroundColor = `rgb(${Math.floor(
+      Math.random() * 255
+    )},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)}`;
   };
-  createDivs(100);
-  
+
+  createDivs(divs);
   return {
-    div : div,
-    bgChange: bgChange
-  }
-}();
+    div,
+    bgChange
+  };
+})();
 
-div.addEventListener("mouseover", bgChange.bgChange);
-
-/*           -- version inicial --
-
-var div = document.getElementById('container');
-function bgChange(e) {
-  var rndCol = 'rgb(' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ')';
-  e.target.style.backgroundColor = rndCol;
-}   
-
-for ( var i = 0; i < 99; i++ ){
-  var newDiv = document.createElement('div');
-  newDiv.classList.add("cube");
-  div.appendChild(newDiv);
-  
-}
-
-div.addEventListener("mouseover", bgChange);
-
-*/
-
+div.addEventListener("mouseover", tecniColor.bgChange);
