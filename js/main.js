45cae31fb902d2aa9b2cc0acbe03e01b5ae14d06
Dom <25825490+domgalop@users.noreply.github.com>
2018-06-13 20:11:30 -0500

    tecnicolor boxes version 1

--- js/main.js
+++ js/main.js
@@ -1,40 +1,11 @@
-const div = document.getElementById('container');
-let bgChange = function() {
-  let fragment = document.createDocumentFragment(),
-  element = undefined,
-  divs = 100;
-
-  let createDivs = (param) => {
-    for (let i = 0; i < param; i++ ){
-      element = document.createElement('div');
-      element.classList.add("cube");
-      fragment.appendChild(element); 
-    }
-    div.appendChild(fragment);
-  };
-
-  let bgChange = (e) => {
-    e.target.style.backgroundColor = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)}`;
-  };
-  createDivs(100);
-  
-  return {
-    div : div,
-    bgChange: bgChange
-  }
-}();
-
-div.addEventListener("mouseover", bgChange.bgChange);
-
-/*           -- version inicial --
-
+/* -- Version 1 -- */
 var div = document.getElementById('container');
 function bgChange(e) {
   var rndCol = 'rgb(' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ')';
   e.target.style.backgroundColor = rndCol;
 }   
 
-for ( var i = 0; i < 99; i++ ){
+for ( var i = 0; i < 100; i++ ){
   var newDiv = document.createElement('div');
   newDiv.classList.add("cube");
   div.appendChild(newDiv);
@@ -42,6 +13,3 @@ for ( var i = 0; i < 99; i++ ){
 }
 
 div.addEventListener("mouseover", bgChange);
-
-*/
-
